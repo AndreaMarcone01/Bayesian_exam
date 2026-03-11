a7374f1edd004e78f8a5d83cfdf42e5c9e19999f
Andrea Marcone <andre@marconenet.it>
2026-03-11 18:10:58 +0100

    Initial commit

---
+++ GRB_classification.py
@@ -0,0 +1,384 @@
+import numpy as np
+import os
+import sys
+
+main_dir = os.path.dirname(os.path.realpath(__file__))
+parentdir = os.path.dirname(main_dir)
+sys.path.insert(0, parentdir) 
+
+from pdf_analysis import errors_around_peak
+
+def gauss(x, mu, sigma):
+    """Log normal model.
+    
+    Args:
+        x (array): normal distributed variable, this is expected as log
+        mu (float): mean of the distribution
+        sigma (float): variance of the distribution
+        
+    Returns:
+        pdf (array): probability density function, normalized to 1
+    """
+
+    pdf = 1/(np.sqrt(2 * np.pi)*sigma) * np.exp(-0.5 * ((x - mu)/sigma)**2)
+    return pdf
+
+def weighted_log_normal(x, theta):
+    """Weighted sum of two log-normal.
+    
+    Args:
+        x (array): independent variable
+        theta (array): parameter array, in order [w, mu_1, sigma_1, mu_2, sigma_2]
+        
+    Returns:
+        total model (array): probability density function, normalized to 1
+    """
+
+    w = theta[0]
+    mu_1 = theta[1]
+    sigma_1 = theta[2]
+    mu_2 = theta[3]
+    sigma_2 = theta[4]
+
+    normal_1 = gauss(x, mu_1, sigma_1)
+    normal_2 = gauss(x, mu_2, sigma_2)
+    model = w * normal_1 + (1-w) * normal_2
+    return model
+
+def log_prior(theta, bounds):
+    """Uniform prior inside the bounds
+    
+    Args:
+        theta (array): parameters on which calculate the posterior [w, mu1, sigma1, mu2, sigma2]
+        bounds (array): bound for each parameter, expected as [min, max]
+        
+    Returns:
+        prior (float): uniform prior
+    """
+    
+    prior = np.zeros(theta.shape[0])
+
+    for i in range(theta.shape[0]):
+        if theta[i] < bounds[i][0] or theta[i] > bounds[i][1]:
+            prior[i] = - np.inf
+    
+    # check if mu_1 < mu_2. System of penalty for mu_1 near mu_2?
+    #if theta[1] > theta[3]:
+    #    prior[1] = - np.inf
+    
+    prior = np.sum(prior)
+    return prior
+
+def log_likelihood(theta, counts, center, model):
+    """Poisson log likelihood for model.
+    
+    Args:
+        theta (array): parameters of the model
+        counts (array): counts of the histogram not normalized
+        center (array): center of the bins of histogram
+        model (function): model to use
+        
+    Returns:
+        likelihood (float): log of the likelihood
+    """
+
+    N = np.sum(counts)
+    dx = np.diff(center)[0]
+    expected_count = N * model(center, theta) * dx
+    log_like = counts * np.log(expected_count) - expected_count
+    return np.sum(log_like)
+
+def log_posterior(theta, counts, center, model, bounds):
+    """Log posterior for model.
+    
+    Args:
+        theta (array): parameters of the model
+        counts (array): counts of the histogram not normalized
+        center (array): center of the bins of histogram
+        model (function): model to use
+        bounds (array): bound for each parameter, expected as [min, max]
+        
+    Returns:
+        posterior (array): log of the posterior
+    """
+    
+    posterior = log_prior(theta, bounds) + log_likelihood(theta, counts, center, model)
+    return posterior
+
+def proposed_distribution(x, bounds, rng, blind = True):
+    """Proposed distribution for the MCMC algorithm.
+    
+    Args:
+        x (array): input of the distribution
+        bound (array): bound for each parameter, expected as [min, max]. Used to define covariance
+        rng (np.random.default_rng): default rng for reproduce results
+        blind (boolean): decide if run with a blind covariance or use a already note covariance
+        
+    Returns:
+        pdf (array): proposed distribution values
+    """
+
+    d = x.shape[0]
+    if blind == False:
+        fname = main_dir+"\\samples_covariance.txt"
+        if os.path.isfile(fname) == True: 
+            covariance = np.loadtxt(fname)
+        else:
+            print("Covariance file not found!")
+            scales = 0.05 * np.diff(bounds)[:,0]
+            covariance = np.diag(scales**2) # diagonal matrix of dimension d*d
+    
+    else:
+        scales = 0.05 * np.diff(bounds)[:,0]
+        covariance = np.diag(scales**2) # diagonal matrix of dimension d*d
+    
+    pdf = rng.multivariate_normal(np.zeros(d), covariance)
+    return pdf
+
+def metropolis_hastings(theta0, postpdf, counts, center, model, bounds, rng, blind, n = 10000):
+    """Metropolis hastings algorithm to sample the posterior.
+    
+    Args:
+        theta0 (array): initial point for the chain
+        postpdf (function): posterior to use for the chain
+        counts (array): counts of the histogram not normalized
+        center (array): center of the bins of histogram
+        model (function): model to use for posterior
+        bounds (array): bound for each parameter, expected as [min, max]
+        rng (np.random.default_rng): default rng for reproduce results
+        blind (boolean): decide if run with a blind covariance or use a already note covariance
+        n (float): length of the chain, default to 10000
+    
+    Returns:
+        samples (array): samples of the parameters
+    """
+
+    d = theta0.shape[0]
+    logP0 = postpdf(theta0, counts, center, model, bounds)
+    samples = np.zeros((n,d), dtype=np.float64)
+    logP = np.zeros(n, dtype=np.float64)
+    accepted = 0
+    rejected = 0
+
+    for i in range(n):
+        theta_try = theta0 + proposed_distribution(theta0, bounds, rng, blind)
+        logP_try = postpdf(theta_try, counts, center, model, bounds)
+        
+        if logP_try - logP0 > np.log(np.random.uniform(0,1)):
+            samples[i,:] = theta_try
+            logP[i] = logP_try
+            theta0 = theta_try
+            logP0 = logP_try
+            accepted += 1
+        else:
+            samples[i,:] = theta0
+            logP[i] = logP0
+            rejected += 1
+        
+        print(f"Iteration {i}, acceptance = {accepted/(accepted+rejected)}")
+
+    return samples, logP
+
+def autocorrelation(x, norm = True):
+    """Find the autocorrelation of x
+    
+    Args:
+        x (array): array on which compute the autocorrelation
+        norm (boolean): decide if normalize or not, default to True
+        
+    Returns:
+        autocorr (array): array of autocorrelation
+    """
+
+    f = np.fft.fft(x - np.mean(x), n = 2*len(x))
+    f_con = np.conjugate(f)
+    corr = np.real(np.fft.ifft(f * f_con)[:len(x)])
+
+    if norm == True:
+        corr /= corr[0]
+        
+    return corr
+
+if __name__ == "__main__":
+    import matplotlib.pyplot as plt
+    import os
+
+    rng = np.random.default_rng(111) # initialize seed for reproducibility
+
+    main_dir = os.path.dirname(os.path.realpath(__file__))
+    log_T90, d_log_T90, Hardness_R = np.loadtxt(main_dir+"\\Data\\GRB_data.txt", unpack=True)
+    
+    print("Minimum is:", np.min(log_T90))
+    print("Maximum is:", np.max(log_T90))
+    print("Difference minimum is:", np.min(np.diff(log_T90)))
+    
+    nbins = 50
+    bins = np.linspace(-4, 7, nbins)
+    print(f"A bin is large {np.diff(bins)[0]:.2e}")
+    hist, edges = np.histogram(log_T90, bins = bins, density = True)    # used in plot
+    counts, _ = np.histogram(log_T90, bins = bins)                      # used for the real analysis
+    center = (edges[1:] + edges[:-1])*0.5
+
+    par_name = np.array(["$w$", "$\\mu_1$", "$\\sigma_1$", "$\\mu_2$", "$\\sigma_2$"])
+    bounds = np.array([[0.0,1.0], [-4.0,7.0], [0.0,3.0] , [-4.0, 7.0], [0.0,3.0]])
+    theta_0 = np.array([rng.uniform(0,1),
+                        rng.uniform(-4,7), 
+                        rng.uniform(0,5),
+                        rng.uniform(-4,7),
+                        rng.uniform(0,3)])
+    theta_fit = np.array([0.4, -0.5, 1.5, 3.6, 0.8])
+
+    xx = np.linspace(-4,7,100)
+    pdf = weighted_log_normal(xx, theta_0)
+    pdf_f = weighted_log_normal(xx, theta_fit)
+
+    """ # plot the data
+    plt.figure("Data and model")
+    plt.plot(center, hist, '.', label = "Data")
+    plt.plot(xx, pdf, 'r', label = "Model, theta random")
+    plt.plot(xx, pdf_f, 'g', label = "Model, theta eye-fitted")
+    #plt.plot(xx, w_normal_2, label = "Norm 2", alpha = 0.5)
+    plt.xlabel("$\\log(T_{90})$")
+    plt.ylabel("Normalized Counts")
+    plt.legend()
+    """
+
+    # try to initialise things
+        
+    run = False
+    prep_run = True
+
+    if run == True:
+        samples, logP = metropolis_hastings(theta_0, log_posterior, counts, center, 
+                                            weighted_log_normal, bounds, rng, blind = True)
+        
+        # use this first estimate to adjust the proposal
+        covariance = np.cov(samples.T)
+        np.savetxt(main_dir+"\\samples_covariance.txt", covariance)
+
+        if prep_run == True:
+            # re-run chain with new covariance in proposal
+            samples, logP = metropolis_hastings(theta_0, log_posterior, counts, center, 
+                                            weighted_log_normal, bounds, rng, blind = False, n=100000)
+        
+        # save the chain
+        header = "w , mu_1, sigma_1, mu_2, sigma_2"
+        np.savetxt(main_dir+"\\samples_chain.txt", samples, header=header)
+        np.savetxt(main_dir+"\\samples_posterior.txt", logP, header="log posterior")
+
+    else: 
+        samples = np.loadtxt(main_dir+"\\samples_chain.txt")
+        logP = np.loadtxt(main_dir+"\\samples_posterior.txt")
+        
+
+    # Look at the results
+    fig = plt.figure("Parameters chain", figsize = (6,6))
+    for i in range(samples.shape[1]):
+        ax = fig.add_subplot(5, 1, i+1)
+        ax.plot(samples[:,i], '.', color = 'C0')
+        ax.axhline(theta_0[i], color = 'orange', label = 'Initial value', linestyle = 'dashed')
+        ax.set_ylabel(par_name[i])
+    ax.set_xlabel("Iteration")
+    plt.tight_layout()
+    plt.savefig(main_dir+"\\Results\\Parameters_chain.png")
+
+    burnin = 250 
+    fig = plt.figure("Parameters chain: zoom to burn-in", figsize = (6,6))
+    for i in range(samples.shape[1]):
+        ax = fig.add_subplot(5, 1, i+1)
+        ax.plot(samples[:,i], '.', color = 'C0')
+        ax.axhline(theta_0[i], color = 'orange', label = 'Initial value', linestyle = 'dashed')
+        ax.axvline(burnin, color = 'r', label = 'Proposed burn-in', linestyle = 'dashed')
+        ax.set_xlim(-50, 2*burnin)
+        ax.set_ylabel(par_name[i])
+    ax.set_xlabel("Iteration")
+    plt.tight_layout()
+    plt.savefig(main_dir+"\\Results\\Parameters_chain_zoom.png")
+
+    """
+    fig = plt.figure("Chain posterior")
+    ax = fig.add_subplot(111)
+    ax.plot(logP, label = 'log Posterior')
+    ax.axvline(400, color = 'r', label = 'Proposed burn-in', linestyle = 'dashed')
+    #ax.axhline(np.percentile(logP, 0.9), color = 'r', label = "50 percentile", linestyle='dashed')
+    ax.set_ylabel("log posterior")
+    ax.set_xlabel("Iteration")
+    plt.legend()
+    """
+    
+    
+    # look at autocorrelation for theta
+    fig = plt.figure("Parameters autocorrelation", figsize = (6,6))
+    for i in range(samples.shape[1]):
+        ax = fig.add_subplot(5, 1, i+1)
+        ax.plot(autocorrelation(samples[:,i]), '.', color = 'C0', label = 'Autocorrelation')
+        ax.set_ylabel(par_name[i])
+    ax.set_xlabel("Iteration")
+    plt.tight_layout()
+    plt.savefig(main_dir+"\\Results\\Parameters_autocorr.png")
+
+    thinning = 100
+    fig = plt.figure("Parameters autocorrelation: zoom to thinning", figsize = (6,6))
+    for i in range(samples.shape[1]):
+        ax = fig.add_subplot(5, 1, i+1)
+        ax.plot(autocorrelation(samples[:,i]), '.', color = 'C0', label = 'Autocorrelation')
+        ax.axvline(thinning, color = 'r', label = 'Proposed thinning', linestyle = 'dashed')
+        ax.set_ylabel(par_name[i])
+        ax.set_xlim(-50, 2 * thinning)
+    ax.set_xlabel("Iteration")
+    plt.tight_layout()
+    plt.savefig(main_dir+"\\Results\\Parameters_autocorr_zoom.png")
+    
+    # after burn-in and autocorrelation we plot the histograms of the parameters
+    parameters = samples[burnin:,:]
+    parameters = parameters[::thinning,:]
+
+    print(f"After burn-in and thinning we have {len(parameters[:,0])} samples")
+    
+    # find peaks and credible interval
+    par_val = np.zeros(len(par_name))
+    d_par_plus = np.zeros(len(par_name))
+    d_par_minus = np.zeros(len(par_name))
+    for i in range(parameters.shape[1]):
+        counts_i, bins_i = np.histogram(parameters[:,i], bins = 30, density=True)
+        center_i = 0.5*(bins_i[1:] + bins_i[:-1])
+        par_val[i], d_par_plus[i], d_par_minus[i] = errors_around_peak(center_i, counts_i)
+
+    header = "par_val d_par_plus d_par_minus"
+    np.savetxt(main_dir+"\\Results\\parameters_values.txt", np.array([par_val, d_par_plus, d_par_minus]).T, header=header)
+
+
+    fig = plt.figure("Parameters histogram", figsize = (6,6))
+    for i in range(parameters.shape[1]):
+        ax = fig.add_subplot(5, 1, i+1)
+        counts_i, bins_i = np.histogram(parameters[:,i], bins = 30, density=True)
+        ax.stairs(counts_i, bins_i, color = 'C0', label = 'Posterior samples', linewidth = 1.5)
+        ax.axhline(1/(bounds[i][1] - bounds[i][0]), color = 'r', label = "Prior", linestyle='dashed')
+        ax.axvline(par_val[i], color = 'green', label = "Peak value", linestyle='dashed')
+        ax.axvline(par_val[i]+d_par_plus[i], color = 'orange', linestyle='dashed')
+        ax.axvline(par_val[i]-d_par_minus[i], color = 'orange', label = "Peak value $\\pm\\sigma$", linestyle='dashed')
+        ax.set_xlabel(par_name[i])
+        ax.set_ylim(0, np.max(counts_i) * 1.1)
+    plt.tight_layout()
+    plt.savefig(main_dir+"\\Results\\Parameters_hist.png")
+
+    
+    pdf = weighted_log_normal(xx, par_val)
+    w_normal_1 = par_val[0] * gauss(xx, par_val[1], par_val[2])
+    w_normal_2 = (1-par_val[0]) * gauss(xx, par_val[3], par_val[4])
+
+    # plot the data with the best fit
+    plt.figure("Data and model")
+    plt.plot(center, hist, '.', label = "Data")
+    plt.plot(xx, pdf, 'r', label = "Model")
+    plt.plot(xx, w_normal_1, 'g', label = "Norm 1", alpha = 0.5)
+    plt.plot(xx, w_normal_2, color = 'orange', label = "Norm 2", alpha = 0.5)
+    plt.xlabel("$\\log(T_{90})$")
+    plt.ylabel("Normalized Counts")
+    plt.legend()
+    plt.savefig(main_dir+"\\Results\\Data_and_model.png")
+
+
+    # plt.show()
+    # it does not found the maxima of the posterior, it wanders around testing all values but not the maximum
+    # tomorrow: plot with new parameters. Burn-in and autocorrelation? 
