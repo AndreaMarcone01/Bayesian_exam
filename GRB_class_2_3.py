9134753fbc32cbcc8db2403ff909b0cd89829e38
Andrea Marcone <andre@marconenet.it>
2026-03-16 10:51:33 +0100

    point 3 done

--- GRB_class_2_3.py
+++ GRB_class_2_3.py
@@ -45,23 +45,16 @@ if __name__ == "__main__":
     rng = np.random.default_rng(111) # initialize seed for reproducibility
 
     main_dir = os.path.dirname(os.path.realpath(__file__))
-    log_T90, d_log_T90, Hardness_R = np.loadtxt(main_dir+"\\Data\\GRB_data.txt", unpack=True)
-
-    nbins = 50
-    bins = np.linspace(-4, 7, nbins)
-    xx = np.linspace(-4,7,100)
-    hist, edges = np.histogram(log_T90, bins = bins, density = True)    # used in plot
-    counts, _ = np.histogram(log_T90, bins = bins)                      # used for the real analysis
-    center = (edges[1:] + edges[:-1])*0.5
 
     # import results of the first point
+    xx = np.linspace(-4,7,256)
     par_val, d_par_plus, d_par_minus = np.loadtxt(main_dir+"\\Results\\parameters_values.txt", unpack = True)
 
     pdf = weighted_log_normal(xx, par_val)
     w_normal_1 = par_val[0] * gauss(xx, par_val[1], par_val[2])
     w_normal_2 = (1-par_val[0]) * gauss(xx, par_val[3], par_val[4])
 
-    # Second point of the exercise: classificate a GRB
+    # Second point of the exercise: classifying a GRB
     log_T = np.log(2.0)
 
     w = par_val[0]
@@ -69,13 +62,13 @@ if __name__ == "__main__":
     normal_2_at_T = gauss(log_T, par_val[3], par_val[4])
     pdf_at_T = weighted_log_normal(log_T, par_val)
     
-    prob_short = w * normal_1_at_T/pdf_at_T
-    prob_long = (1-w) * normal_2_at_T/pdf_at_T
-    print(f"Probability that is short: {prob_short}")
-    print(f"Probability that is long: {prob_long}")
-    print(f"Probability that is long or short: {prob_short+prob_long}")
+    prob_short_at_T = w * normal_1_at_T/pdf_at_T
+    prob_long_at_T = (1-w) * normal_2_at_T/pdf_at_T
+    print(f"Probability that is short: {prob_short_at_T:.3f}")
+    print(f"Probability that is long: {prob_long_at_T:.3f}")
+    print(f"Probability that is long or short: {prob_short_at_T+prob_long_at_T}")
 
-    plt.figure("Model with GRB to classificate")
+    plt.figure("Model with GRB to classifying")
     plt.plot(xx, pdf, 'C0', label = "Model")
     plt.axvline(log_T, color = 'r', label = "GRB170817A", linestyle = 'dashed')
     plt.plot(xx, w_normal_1, 'g', label = "Norm 1", alpha = 0.5)
@@ -83,4 +76,41 @@ if __name__ == "__main__":
     plt.xlabel("$\\log(T_{90})$")
     plt.ylabel("Normalized Counts")
     plt.legend()
+    plt.savefig(main_dir+"\\Results\\GRB_to_class.png")
+
+    # Point 3: decide a figure of merit for the transition between long and short GRBs
+    prob_short = w_normal_1/pdf
+    prob_long = w_normal_2/pdf
+    delta = np.abs(prob_short-prob_long)
+
+    # find the transition point
+    log_T_trans_plus = xx[prob_short < prob_long][0]
+    log_T_trans_minus = xx[prob_short > prob_long][-1]
+    log_T_trans = np.mean([log_T_trans_minus, log_T_trans_plus])
+    print(f"The transition point is at logT = {log_T_trans:.2f}, so for T = {np.exp(log_T_trans):.2f} s")
+
+    begin_p = xx[delta<0.9][0]
+    delta_p = delta[xx<begin_p]
+    begin_m = xx[xx<begin_p][delta_p>0.9][-1]
+    begin = np.mean([begin_p, begin_m])
+    print(f"The transition begins at logT = {begin:.2f}, so T = {np.exp(begin):.2f} s")
+
+    end_m = xx[delta<0.9][-1]
+    delta_m = delta[xx>end_m]
+    end_p = xx[xx>end_m][delta_m>0.9][0]
+    end = np.mean([end_p, end_m])
+    print(f"The transition ends at logT = {end:.2f}, so T = {np.exp(end):.2f} s")
+
+    plt.figure("Probabilities for the two class of GRB")
+    plt.plot(xx, prob_short, 'g', label = "P short")
+    plt.plot(xx, prob_long, color = 'orange', label = "P long")
+    plt.axvline(log_T_trans, color = 'r', label = "Threshold", linestyle = 'dashed')
+    plt.axvline(begin, color = 'b', linestyle = 'dashed', alpha = 0.5)
+    plt.axvline(end, color = 'b', label = "Transition", linestyle = 'dashed', alpha = 0.5)
+    plt.xlabel("$\\log(T_{90})$")
+    plt.xlim([-4,7])
+    plt.ylabel("Probability")
+    plt.grid(linestyle = 'dashed')
+    plt.legend()
+    plt.savefig(main_dir+"\\Results\\Porb_of_class.png")
     plt.show()
