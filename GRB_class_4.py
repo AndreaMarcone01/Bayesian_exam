0689c69078bb488a3b8e56d540695cbbdf0b4a69
Andrea Marcone <andre@marconenet.it>
2026-04-07 12:42:20 +0200

    07/04: reorder plots point 4

--- GRB_class_4.py
+++ GRB_class_4.py
@@ -116,10 +116,8 @@ if __name__ == "__main__":
     
     xx = np.linspace(-4,7,256)
     
-    # take sample and evidence from the saved file
-    """
-    # first for the case of uniform priors, then Jeffereys
-    
+    # Analyise the results
+    # I'm going with Jeffreys priors
     filename = os.path.join("two_class_output","raynest.h5")
     h5_file = h5py.File(filename,'r')
     samples2 = h5_file['combined'].get('posterior_samples')
@@ -130,8 +128,8 @@ if __name__ == "__main__":
     samples3 = h5_file['combined'].get('posterior_samples')
     log_evidence3 = h5_file['combined'].get('logZ')[()]
 
-    print(f"The Odd ratio is O_23 = {np.exp(log_evidence2-log_evidence3):.2e} (uniform priors)")
-
+    print(f"The Odd ratio is O_23 = {np.exp(log_evidence2-log_evidence3):.2e}")
+    
     # first of all: trace of the samples, burnin and thinning
     # change from array of tuples to array of arrays
     samples2 = samples2[()]
@@ -148,206 +146,12 @@ if __name__ == "__main__":
         ax.set_ylabel(name2_l[i])
     ax.set_xlabel("Iteration")
     plt.tight_layout()
-
-    # honestly burn-in = 0
-
-    # look at autocorrelation for theta
-    fig = plt.figure("Parameters autocorrelation 2", figsize = (6,6))
-    for i in range(samples2.shape[1]):
-        ax = fig.add_subplot(5, 1, i+1)
-        ax.plot(autocorrelation(samples2[:,i]), '.', color = 'C0', label = 'Autocorrelation')
-        ax.set_ylabel(name2_l[i])
-    ax.set_xlabel("Iteration")
-    plt.tight_layout()
-
-    thinning = 10
-    parameters = samples2[::thinning,:]
-
-    # find peaks and credible interval
-    par_val = np.zeros(len(name2))
-    d_par_plus = np.zeros(len(name2))
-    d_par_minus = np.zeros(len(name2))
-    for i in range(parameters.shape[1]):
-        counts_i, bins_i = np.histogram(parameters[:,i], bins = 30, density=True)
-        center_i = 0.5*(bins_i[1:] + bins_i[:-1])
-        par_val[i], d_par_plus[i], d_par_minus[i] = errors_around_peak(center_i, counts_i)
-
-    header = "par_val d_par_plus d_par_minus"
-    np.savetxt(main_dir+"\\Results\\NS\\2_parameters_values.txt", np.array([par_val, d_par_plus, d_par_minus]).T, header=header)
-
-
-    fig = plt.figure("Parameters histogram 2", figsize = (6,6))
-    for i in range(parameters.shape[1]):
-        ax = fig.add_subplot(5, 1, i+1)
-        counts_i, bins_i = np.histogram(parameters[:,i], bins = 30, density=True)
-        ax.stairs(counts_i, bins_i, color = 'C0', label = 'Posterior samples', linewidth = 1.5)
-        ax.axhline(1/(bounds2[i][1] - bounds2[i][0]), color = 'r', label = "Prior", linestyle='dashed')
-
-        ax.axvline(par_val[i], color = 'g', label = "Peak value", linestyle='dashed')
-        ax.axvline(par_val[i]+d_par_plus[i], color = 'orange', linestyle='dashed')
-        ax.axvline(par_val[i]-d_par_minus[i], color = 'orange', label = "Peak value $\\pm\\sigma$", linestyle='dashed')
-        ax.set_xlabel(name2_l[i])
-        ax.set_xlim(np.min(bins_i)*0.9, np.max(bins_i)*1.1)
-        ax.set_ylim(0, np.max(counts_i) * 1.1)
-    plt.tight_layout()
-    plt.savefig(main_dir+"\\Results\\NS\\2_Parameters_histogram.png", dpi = 600)
-
-
-    posterior_models = [weighted_log_normal(xx, s) for s in parameters]
-    l, pdf2, h = np.percentile(posterior_models,[16,50,84],axis=0)
-    w_normal_1 = np.percentile([s[0] * gauss(xx, s[1], s[2]) for s in parameters], 50, axis=0)
-    w_normal_2 = np.percentile([(1-s[0]) * gauss(xx, s[3], s[4]) for s in parameters], 50, axis=0)
-
-    # plot the data with the best fit
-    plt.figure("Data and model 2")
-    plt.stairs(hist, edges, color = 'C0', label = 'Data')
-    plt.plot(xx, pdf2, 'r', label = "Model")
-    plt.fill_between(xx, h, l, facecolor='tomato', alpha = 0.5)
-    plt.plot(xx, w_normal_1, 'g', label = "Norm 1", alpha = 0.5)
-    plt.plot(xx, w_normal_2, color = 'orange', label = "Norm 2", alpha = 0.5)
-    plt.xlabel("$\\log(T_{90})$")
-    plt.ylabel("Normalized Counts")
-    plt.legend()
-    plt.grid(linestyle = 'dashed')
-    plt.savefig(main_dir+"\\Results\\NS\\2_Data_and_model.png", dpi = 600)
-
-    # now all again for model with 3 class
-    samples3 = samples3[()]
-    sample = np.zeros((len(samples3),len(name3)))
-    for i in range(len(name3)):
-        sample[:,i] = samples3[name3[i]]
-    samples3 = sample
-
-    # trace plots
-    fig = plt.figure("Parameters chain 3", figsize = (6,6))
-    for i in range(samples3.shape[1]):
-        ax = fig.add_subplot(8, 1, i+1)
-        ax.plot(samples3[:,i], '.', color = 'C0')
-        ax.set_ylabel(name3_l[i])
-    ax.set_xlabel("Iteration")
-    plt.tight_layout()
-    
-    burnin = 1000
-
-    # look at autocorrelation for theta
-    fig = plt.figure("Parameters autocorrelation 3", figsize = (6,6))
-    for i in range(samples3.shape[1]):
-        ax = fig.add_subplot(8, 1, i+1)
-        ax.plot(autocorrelation(samples3[:,i]), '.', color = 'C0', label = 'Autocorrelation')
-        ax.set_ylabel(name3_l[i])
-    ax.set_xlabel("Iteration")
-    plt.tight_layout()
-
-    thinning = 10
-    sample = samples3[burnin:,:]
-    parameters = sample[::thinning,:]
-
-    # find peaks and credible interval
-    par_val = np.zeros(len(name3))
-    d_par_plus = np.zeros(len(name3))
-    d_par_minus = np.zeros(len(name3))
-    for i in range(parameters.shape[1]):
-        counts_i, bins_i = np.histogram(parameters[:,i], bins = 30, density=True)
-        center_i = 0.5*(bins_i[1:] + bins_i[:-1])
-        par_val[i], d_par_plus[i], d_par_minus[i] = errors_around_peak(center_i, counts_i)
-
-    header = "par_val d_par_plus d_par_minus"
-    np.savetxt(main_dir+"\\Results\\NS\\3_parameters_values.txt", np.array([par_val, d_par_plus, d_par_minus]).T, header=header)
-
-
-    fig = plt.figure("Parameters histogram 3", figsize = (6,6))
-    for i in range(parameters.shape[1]):
-        if i == 7:
-            ax = fig.add_subplot(4, 2, 2)
-        elif i == 0:
-            ax = fig.add_subplot(4, 2, 1)
-        else:
-            ax = fig.add_subplot(4, 2, i+2)
-        counts_i, bins_i = np.histogram(parameters[:,i], bins = 30, density=True)
-        ax.stairs(counts_i, bins_i, color = 'C0', label = 'Posterior samples', linewidth = 1.5)
-        ax.axhline(1/(bounds3[i][1] - bounds3[i][0]), color = 'r', label = "Prior", linestyle='dashed')
-        ax.axvline(par_val[i], color = 'g', label = "Median value", linestyle='dashed')
-        ax.axvline(par_val[i]+d_par_plus[i], color = 'orange', linestyle='dashed')
-        ax.axvline(par_val[i]-d_par_minus[i], color = 'orange', label = "Peak value $\\pm\\sigma$", linestyle='dashed')
-        ax.set_xlabel(name3_l[i])
-        ax.set_xlim(np.min(bins_i)*0.9, np.max(bins_i)*1.1)
-        ax.set_ylim(0, np.max(counts_i) * 1.1)
-    plt.tight_layout()
-    plt.savefig(main_dir+"\\Results\\NS\\3_Parameters_histogram.png", dpi = 600)
-
-    posterior_models = [three_weighted_log_normal(xx, s) for s in parameters]
-    l, pdf3, h = np.percentile(posterior_models,[16,50,84],axis=0)
-    w_normal_1 = np.percentile([s[0] * gauss(xx, s[1], s[2]) for s in parameters], 50, axis=0)
-    w_normal_2 = np.percentile([s[7] * gauss(xx, s[3], s[4]) for s in parameters], 50, axis=0)
-    w_normal_3 = np.percentile([(1-s[0]-s[7]) * gauss(xx, s[5], s[6]) for s in parameters], 50, axis=0)
-
-    # plot the data with the best fit
-    plt.figure("Data and model 3")
-    plt.stairs(hist, edges, color = 'C0', label = 'Data')
-    plt.plot(xx, pdf3, 'r', label = "Model")
-    plt.fill_between(xx, h, l, facecolor='tomato', alpha = 0.5)
-    plt.plot(xx, w_normal_1, 'g', label = "Norm 1", alpha = 0.5)
-    plt.plot(xx, w_normal_2, 'purple', label = "Norm 2", alpha = 0.5)
-    plt.plot(xx, w_normal_3, color = 'orange', label = "Norm 3", alpha = 0.5)
-    plt.xlabel("$\\log(T_{90})$")
-    plt.ylabel("Normalized Counts")
-    plt.legend()
-    plt.grid(linestyle = 'dashed')
-    plt.savefig(main_dir+"\\Results\\NS\\3_Data_and_model.png", dpi = 600)
-    
-    # plot the data with the two best fit
-    plt.figure("Data and the two models")
-    plt.stairs(hist, edges, color = 'C0', label = 'Data')
-    plt.plot(xx, pdf2, 'k', label = "Model with 2 classes")
-    plt.plot(xx, pdf3, 'r', label = "Model with 3 classes")
-    plt.xlabel("$\\log(T_{90})$")
-    plt.ylabel("Normalized Counts")
-    plt.legend()
-    plt.grid(linestyle = 'dashed')
-    plt.savefig(main_dir+"\\Results\\NS\\Model_both.png", dpi = 600)
-
-    #plt.show()
-    #exit()
-
-    plt.close('all')
-    """
-
-    #I'm going with Jeffreys priors
-
-    filename = os.path.join("J_two_class_output","raynest.h5")
-    h5_file = h5py.File(filename,'r')
-    samples2 = h5_file['combined'].get('posterior_samples')
-    log_evidence2 = h5_file['combined'].get('logZ')[()]
-    
-    filename = os.path.join("J_three_class_output","raynest.h5")
-    h5_file = h5py.File(filename,'r')
-    samples3 = h5_file['combined'].get('posterior_samples')
-    log_evidence3 = h5_file['combined'].get('logZ')[()]
-
-    print(f"The Odd ratio is O_23 = {np.exp(log_evidence2-log_evidence3):.2e}")
-    
-    # first of all: trace of the samples, burnin and thinning
-    # change from array of tuples to array of arrays
-    samples2 = samples2[()]
-    sample = np.zeros((len(samples2),5))
-    for i in range(len(name2)):
-        sample[:,i] = samples2[name2[i]]
-    samples2 = sample
-
-    # trace plots
-    fig = plt.figure("J Parameters chain 2", figsize = (6,6))
-    for i in range(samples2.shape[1]):
-        ax = fig.add_subplot(5, 1, i+1)
-        ax.plot(samples2[:,i], '.', color = 'C0')
-        ax.set_ylabel(name2_l[i])
-    ax.set_xlabel("Iteration")
-    plt.tight_layout()
     
     burnin = 100
     samples2 = samples2[burnin:,:]
 
     # look at autocorrelation for theta
-    fig = plt.figure("J Parameters autocorrelation 2", figsize = (6,6))
+    fig = plt.figure("Parameters autocorrelation 2", figsize = (6,6))
     for i in range(samples2.shape[1]):
         ax = fig.add_subplot(5, 1, i+1)
         ax.plot(autocorrelation(samples2[:,i]), '.', color = 'C0', label = 'Autocorrelation')
@@ -370,7 +174,7 @@ if __name__ == "__main__":
         par_val[i], d_par_plus[i], d_par_minus[i] = errors_around_peak(center_i, counts_i)
 
     
-    fig1 = plt.figure("J Parameters histogram 2", figsize = (6,6))
+    fig1 = plt.figure("Parameters histogram 2", figsize = (6,6))
     for i in range(parameters.shape[1]):
         if i > 0:
             ax = fig1.add_subplot(3, 2, i+2)
@@ -407,7 +211,7 @@ if __name__ == "__main__":
 
     scale = np.diff(bins)[0]*np.sum(counts)
     # plot the data with the best fit
-    fig2 = plt.figure("J Data and model 2")
+    fig2 = plt.figure("Data and model 2")
     ax = fig2.add_subplot(111)
     ax.stairs(counts, edges, color = 'C0', label = 'Data', linewidth=1.5, zorder =1)
     ax.plot(xx, pdf2*scale, 'r', label = "Model",zorder=4)
@@ -433,7 +237,7 @@ if __name__ == "__main__":
     samples3 = sample
 
     # trace plots
-    fig = plt.figure("J Parameters chain 3", figsize = (6,6))
+    fig = plt.figure("Parameters chain 3", figsize = (6,6))
     for i in range(samples3.shape[1]):
         ax = fig.add_subplot(8, 1, i+1)
         ax.plot(samples3[:,i], '.', color = 'C0')
@@ -444,7 +248,7 @@ if __name__ == "__main__":
     burnin = 1000
 
     # look at autocorrelation for theta
-    fig = plt.figure("J Parameters autocorrelation 3", figsize = (6,6))
+    fig = plt.figure("Parameters autocorrelation 3", figsize = (6,6))
     for i in range(samples3.shape[1]):
         ax = fig.add_subplot(8, 1, i+1)
         ax.plot(autocorrelation(samples3[:,i]), '.', color = 'C0', label = 'Autocorrelation')
@@ -468,7 +272,7 @@ if __name__ == "__main__":
         par_val[i], d_par_plus[i], d_par_minus[i] = errors_around_peak(center_i, counts_i)
 
 
-    fig3 = plt.figure("J Parameters histogram 3", figsize = (6,6))
+    fig3 = plt.figure("Parameters histogram 3", figsize = (6,6))
     for i in range(parameters.shape[1]):
         if i == 7:
             ax = fig3.add_subplot(4, 2, 2)
@@ -497,7 +301,7 @@ if __name__ == "__main__":
 
     
     posterior_models = [three_weighted_log_normal(xx, s) for s in parameters]
-    l, pdf3, h = np.percentile(posterior_models,[16,50,84],axis=0)
+    l, pdf3, h = np.percentile(posterior_models,[5,50,95],axis=0)
     w_normal_1 = np.percentile([s[0] * gauss(xx, s[1], s[2]) for s in parameters], 50, axis=0)
     w_normal_2 = np.percentile([s[7] * gauss(xx, s[3], s[4]) for s in parameters], 50, axis=0)
     w_normal_3 = np.percentile([(1-s[0]-s[7]) * gauss(xx, s[5], s[6]) for s in parameters], 50, axis=0)
@@ -505,7 +309,7 @@ if __name__ == "__main__":
     # plot the data with the best fit
     scale = np.diff(bins)[0]*np.sum(counts)
     # plot the data with the best fit
-    fig4 = plt.figure("J Data and model 3")
+    fig4 = plt.figure("Data and model 3")
     ax = fig4.add_subplot(111)
     ax.stairs(counts, edges, color = 'C0', label = 'Data', linewidth=1.5, zorder =1)
     ax.plot(xx, pdf3*scale, 'r', label = "Model",zorder=4)
@@ -520,10 +324,9 @@ if __name__ == "__main__":
     ax.set_axisbelow(True)
     plt.legend()
     
-    plt.close('all')
     
     # plot the data with the two best fit
-    fig5 = plt.figure("J Data and the two models")
+    fig5 = plt.figure("Data and the two models")
     ax = fig5.add_subplot(111)
     ax.stairs(counts, edges, color = 'C0', label = 'Data', linewidth=1.5, zorder =1)
     #ax.fill_between(xx, pdf2*scale, pdf3*scale, color = 'g', alpha = 0.5)
@@ -538,19 +341,19 @@ if __name__ == "__main__":
 
     # save the results
     
-    plt.show()
-    exit()
+    #plt.show()
+    #exit()
 
     header = "par_val d_par_plus d_par_minus"
     # from the 2 class analysis
-    fig1.savefig(main_dir+"\\Results\\NS\\J_2_Parameters_histogram.png", dpi = 600)
-    fig2.savefig(main_dir+"\\Results\\NS\\J_2_Data_and_model.png", dpi = 600)
-    np.savetxt(main_dir+"\\Results\\NS\\J_2_parameters_values.txt", save2, header=header)
+    fig1.savefig(main_dir+"\\Results\\4\\2_Parameters_histogram.png", dpi = 600)
+    fig2.savefig(main_dir+"\\Results\\4\\2_Data_and_model.png", dpi = 600)
+    np.savetxt(main_dir+"\\Results\\4\\2_parameters_values.txt", save2, header=header)
 
     # from the 3 class analysis
-    fig3.savefig(main_dir+"\\Results\\NS\\J_3_Parameters_histogram.png", dpi = 600)
-    fig4.savefig(main_dir+"\\Results\\NS\\J_3_Data_and_model.png", dpi = 600)
-    np.savetxt(main_dir+"\\Results\\NS\\J_3_parameters_values.txt", np.array([par_val, d_par_plus, d_par_minus]).T, header=header)
+    fig3.savefig(main_dir+"\\Results\\4\\3_Parameters_histogram.png", dpi = 600)
+    fig4.savefig(main_dir+"\\Results\\4\\3_Data_and_model.png", dpi = 600)
+    np.savetxt(main_dir+"\\Results\\4\\3_parameters_values.txt", np.array([par_val, d_par_plus, d_par_minus]).T, header=header)
 
     # both models
-    fig5.savefig(main_dir+"\\Results\\NS\\J_Model_both.png", dpi = 600)
+    fig5.savefig(main_dir+"\\Results\\4\\Model_both.png", dpi = 600)
